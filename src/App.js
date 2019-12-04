7a13dba6048dae01db96ab78c34739d36bc619c3
daniel-dantas <ddipaumirim@gmail.com>
2019-12-04 16:07:52 -0300

    Criação do componente Navbar

--- src/App.js
+++ src/App.js
@@ -1,6 +1,20 @@
-import React from 'react';
+import React, {useState} from 'react';
 import Routes from './routes'
+import NavBar from './components/NavBar'
 
-const App = () => <Routes />
+const App = () => {
+
+    const [links] = useState([
+        {caminho: '#', nome: 'Sobre'}
+    ])
+
+    return (
+        <div id="app">
+            <NavBar urlLogo="./assets/logo.png" links={links}/>
+            <Routes />
+        </div>
+    )
+    
+}
 
 export default App;
---
+++ src/components/NavBar/index.js
@@ -0,0 +1,27 @@
+import React from 'react'
+
+const NavBar = (props) => {
+    return (
+        <div id="nav">
+            <nav>
+                <div className="nav-wrapper grey darken-4">
+                    <div className="row">
+                        <div className="col s12 m2 center">
+                            <a  href="/" className="brand-logo"><img id="logo" src={props.urlLogo}/></a>
+                        </div>
+                        <div className="col s12 m10">
+                            <a href="#" data-target="mobile-demo"  className="sidenav-trigger menu"><i className="material-icons">menu</i></a>
+                            <ul className="right hide-on-med-and-down">
+                                <li><a href="/">Pagina Inicial</a></li>
+                                {props.links.map( link => (<li key={link.nome}><a href={link.caminho}>{link.nome}</a></li>))}
+                            </ul>
+                        </div>
+
+                    </div>
+                </div>
+            </nav>
+        </div>
+    )
+}
+
+export default NavBar
